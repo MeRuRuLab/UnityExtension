4bca7507a5b392ec2a9d68a9970c68578229b54c
MeRuRuLab <350969617@qq.com>
2018-02-07 11:35:06 +0800

    Initial commit

---
+++ app/src/main/java/com/meruru/unityextension/MeRuRuActivity.java
@@ -0,0 +1,13 @@
+package com.meruru.unityextension;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeRuRuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_ru_ru);
+    }
+}
