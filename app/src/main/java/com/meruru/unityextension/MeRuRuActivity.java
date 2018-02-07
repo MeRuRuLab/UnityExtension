babff662ae5b9745b232ad80c395a97477c72708
MeRuRuLab <350969617@qq.com>
2018-02-07 12:00:55 +0800

    1
    1

--- app/src/main/java/com/meruru/unityextension/MeRuRuActivity.java
+++ app/src/main/java/com/meruru/unityextension/MeRuRuActivity.java
@@ -1,13 +1,62 @@
 package com.meruru.unityextension;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
-public class MeRuRuActivity extends AppCompatActivity {
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class MeRuRuActivity extends UnityPlayerActivity {
+    Bitmap
+    private IMeRuRuActivity IMRRA;
+    public void AddUnityCallBack(IMeRuRuActivity imrra)
+    {
+        if(imrra!=null)
+        {
+            IMRRA=imrra;
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_me_ru_ru);
+        if (IMRRA!=null)
+        {
+            IMRRA.onCreate(savedInstanceState);
+        }
+        else
+        {
+            Log.w("MeRuRu", "onCreate is null for Unity. ");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (IMRRA!=null)
+        {
+            IMRRA.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        }
+        else
+        {
+            Log.w("MeRuRu", "onRequestPermissionsResult is null for Unity. ");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (IMRRA!=null)
+        {
+            IMRRA.onActivityResult(requestCode,resultCode,data);
+        }
+        else
+        {
+            Log.w("MeRuRu", "onActivityResult is null for Unity. ");
+        }
     }
 }
